d10ce9f05687daaeaee7ce11e657a45aff206fc8
Jimmy Schuurmans <jimmysch123@gmail.com>
2021-03-05 15:40:09 +0100

    [Add] Player + movement
    Added a player sprite that can be controlled with keyboard but working on making it properly OOP

--- Pantheon/Pantheon/Game1.cs
+++ Pantheon/Pantheon/Game1.cs
@@ -6,6 +6,17 @@ namespace Pantheon
 {
     public class Game1 : Game
     {
+        int Slow = 50;
+
+        Texture2D Background;
+        Vector2 BackgroundPosition;
+
+        Texture2D Char;
+        Vector2 CharPosition;
+        float CharSpeed;
+        float CharWalkSpeed = 150f;
+        float CharRunSpeed = 225f;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -19,6 +30,8 @@ namespace Pantheon
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            CharPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2);
+            BackgroundPosition = new Vector2(1000, 1000);
 
             base.Initialize();
         }
@@ -28,6 +41,9 @@ namespace Pantheon
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            Char = Content.Load<Texture2D>("sprites/player");
+            Background = Content.Load<Texture2D>("background/background");
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -36,6 +52,26 @@ namespace Pantheon
                 Exit();
 
             // TODO: Add your update logic here
+            var kstate = Keyboard.GetState();
+
+            if (kstate.IsKeyDown(Keys.W))
+                CharPosition.Y -= CharSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.S))
+                CharPosition.Y += CharSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.A))
+                CharPosition.X -= CharSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (kstate.IsKeyDown(Keys.D))
+                CharPosition.X += CharSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (kstate.IsKeyDown(Keys.LeftShift))
+                CharSpeed = CharRunSpeed;
+            else
+                CharSpeed = CharWalkSpeed;
+
+            if (kstate.IsKeyDown(Keys.LeftControl) && kstate.IsKeyDown(Keys.LeftShift))
+                CharSpeed = CharRunSpeed - Slow;
+            else if (kstate.IsKeyDown(Keys.LeftControl))
+                CharSpeed = CharWalkSpeed - Slow;
 
             base.Update(gameTime);
         }
@@ -45,6 +81,19 @@ namespace Pantheon
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(Background, new Rectangle(0, 0, 800, 420), Color.White);
+            _spriteBatch.Draw(
+                Char,
+                CharPosition,
+                null,
+                Color.White,
+                0f,
+                new Vector2(Char.Width / 2, Char.Height / 2),
+                Vector2.One,
+                SpriteEffects.None,
+                0f);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
---
+++ Pantheon/Pantheon/Sprites/Sprite.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pantheon.Sprites
+{
+    public class Sprite : Component
+    {
+        Texture2D Background;
+        Vector2 BackgroundPosition;
+
+        Texture2D Char;
+        Vector2 CharPosition;
+
+        public Vector2 Position { get; set; }
+
+        public Rectangle rectangle
+        {
+            get { return new Rectangle((int)Position.X, (int)Position.Y, Char.Width, Char.Height); }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Char, Position, Color.White);
+        }
+
+        public Sprite(Texture2D texture)
+        {
+            Char = texture;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+    }
+}
